fb45d3862d8c6d6edc32a40016604985e18a39c5
aliff <alifzulkifeli@gmail.com>
2022-01-23 16:18:50 +0800

    gg

--- pages/index.js
+++ pages/index.js
@@ -1,71 +1,81 @@
-import PostFeed from '../components/PostFeed';
-import Metatags from '../components/Metatags';
-import Loader from '../components/Loader';
-import { firestore, fromMillis, postToJSON } from '../lib/firebase';
+import PostFeed from "../components/PostFeed";
+import Metatags from "../components/Metatags";
+import Loader from "../components/Loader";
+import { firestore, fromMillis, postToJSON } from "../lib/firebase";
 
-import { useState } from 'react';
+import { useState } from "react";
 
 // Max post to query per page
 const LIMIT = 10;
 
 export async function getServerSideProps(context) {
-  const postsQuery = firestore
-    .collectionGroup('posts')
-    .where('published', '==', true)
-    .orderBy('createdAt', 'desc')
-    .limit(LIMIT);
+	const postsQuery = firestore
+		.collectionGroup("posts")
+		.where("published", "==", true)
+		.orderBy("createdAt", "desc")
+		.limit(LIMIT);
 
-  const posts = (await postsQuery.get()).docs.map(postToJSON);
+	const posts = (await postsQuery.get()).docs.map(postToJSON);
 
-  return {
-    props: { posts }, // will be passed to the page component as props
-  };
+	return {
+		props: { posts }, // will be passed to the page component as props
+	};
 }
 
 export default function Home(props) {
-  const [posts, setPosts] = useState(props.posts);
-  const [loading, setLoading] = useState(false);
+	const [posts, setPosts] = useState(props.posts);
+	const [loading, setLoading] = useState(false);
 
-  const [postsEnd, setPostsEnd] = useState(false);
+	const [postsEnd, setPostsEnd] = useState(false);
 
-  // Get next page in pagination query
-  const getMorePosts = async () => {
-    setLoading(true);
-    const last = posts[posts.length - 1];
+	// Get next page in pagination query
+	const getMorePosts = async () => {
+		setLoading(true);
+		const last = posts[posts.length - 1];
 
-    const cursor = typeof last.createdAt === 'number' ? fromMillis(last.createdAt) : last.createdAt;
+		const cursor =
+			typeof last.createdAt === "number"
+				? fromMillis(last.createdAt)
+				: last.createdAt;
 
-    const query = firestore
-      .collectionGroup('posts')
-      .where('published', '==', true)
-      .orderBy('createdAt', 'desc')
-      .startAfter(cursor)
-      .limit(LIMIT);
+		const query = firestore
+			.collectionGroup("posts")
+			.where("published", "==", true)
+			.orderBy("createdAt", "desc")
+			.startAfter(cursor)
+			.limit(LIMIT);
 
-    const newPosts = (await query.get()).docs.map((doc) => doc.data());
+		const newPosts = (await query.get()).docs.map((doc) => doc.data());
 
-    setPosts(posts.concat(newPosts));
-    setLoading(false);
+		setPosts(posts.concat(newPosts));
+		setLoading(false);
 
-    if (newPosts.length < LIMIT) {
-      setPostsEnd(true);
-    }
-  };
+		if (newPosts.length < LIMIT) {
+			setPostsEnd(true);
+		}
+	};
 
-  return (
-    <main>
-      <Metatags title="Home Page" description="Get the latest posts on our site" />
+	return (
+		<main>
+			<Metatags
+				title="Home Page"
+				description="Get the latest posts on our site"
+			/>
 
+			<PostFeed posts={posts} />
 
-      <PostFeed posts={posts} />
+			{!loading && !postsEnd && (
+				<button
+					onClick={getMorePosts}
+					class="py-2 px-4 bg-transparent text-blue-600 font-semibold border border-blue-600 rounded hover:bg-white hover:text-blue-600 hover:border-transparent transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0"
+				>
+					Load more..
+				</button>
+			)}
 
-      {!loading && !postsEnd &&  <button onClick={getMorePosts} class="py-2 px-4 bg-transparent text-blue-600 font-semibold border border-blue-600 rounded hover:bg-blue-600 hover:text-white hover:border-transparent transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0">
-        Load more..
-    </button>}
+			<Loader show={loading} />
 
-      <Loader show={loading} />
-
-      {postsEnd && 'You have reached the end!'}
-    </main>
-  );
+			{postsEnd && "You have reached the end!"}
+		</main>
+	);
 }
